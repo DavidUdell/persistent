697d079e7dd40735fc30f220074615b53eef67dc
DavidUdell <udelldavidb@gmail.com>
2024-09-19 18:57:39 -0700

    Some work with playwright.

--- src/chat_loop.py
+++ src/chat_loop.py
@@ -1,5 +1,9 @@
 # %%
-"""A persistent chatbot via system prompts, with brower API access."""
+"""
+A persistent chatbot via system prompts, with brower API access.
+
+Note that the sync playwright API will not run in a notebook environment.
+"""
 
 
 import os
@@ -8,5 +12,29 @@ import openai
 from playwright.sync_api import sync_playwright
 
 # %%
-# Constants.
+# Constants
 openai.api_key = os.getenv("OPENAI_API_KEY")
+# System prompt
+messages: dict = {
+    "role": "system",
+    "content": """You are an AI with `playwright` API browser access.""",
+}
+
+
+# %%
+# Browser setup
+def run(pwrite):
+    """Run playwright browser setup."""
+
+    browser = pwrite.chromium.launch(headless=False)
+    page = browser.new_page()
+    page.goto("https://www.google.com")
+    print((page.title()))
+
+    browser.close()
+
+
+run(sync_playwright().start())
+
+# %%
+# Interaction loop
