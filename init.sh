4447740f870018a7d0c3fe165c11163674f90939
DavidUdell <udelldavidb@gmail.com>
2024-10-17 17:14:35 -0700

    Automated secrets management.

--- init.sh
+++ init.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+# Export secret, when present
+if [ -f ./.secrets/secret_api_export.sh ]; then
+    source ./.secrets/secret_api_export.sh
+fi
+
 # Docker image and container management
 docker pull ghcr.io/davidudell/persistent:latest
 docker system prune -f
-docker run -it ghcr.io/davidudell/persistent:latest
+docker run -it -e OPENAI_API_KEY=$OPENAI_API_KEY ghcr.io/davidudell/persistent:latest
