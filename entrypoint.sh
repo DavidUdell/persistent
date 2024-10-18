1a3d698a9f4d2ee1b42959bdad5ab44ac1098f98
DavidUdell <udelldavidb@gmail.com>
2024-10-18 16:47:47 -0700

    xclock runs correctly from inside of the container; removing it now.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 set -e
 
-# In-container X11 setup.
+# In-container X11 setup; not using for now.
 # export DISPLAY=:99
 # Xvfb :99 -screen 0 1024x768x16 &
 
 api_key=$OPENAI_API_KEY
 len_api_key=${#api_key}
 
-apt install x11-apps -y
-
 # Ensure the key is not empty.
 if [ $len_api_key -gt 1 ]; then
-    xclock
     python3 src/browser_loop.py
 fi
 
