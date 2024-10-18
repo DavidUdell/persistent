22f000405a571904e02709ff975b18d0b9582a02
DavidUdell <udelldavidb@gmail.com>
2024-10-18 15:10:52 -0700

    Fiddled with entrypoint and onstart scripts, so that X11 will hook up to the outside display and that the loop will launch automatically if the API key was set beforehand.

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,12 @@ export DISPLAY=:99
 
 Xvfb :99 -screen 0 1024x768x16 &
 
+api_key=$OPENAI_API_KEY
+len_api_key=${#api_key}
+
+# Ensure the key is not empty.
+if [ $len_api_key -gt 1 ]; then
+    python3 src/browser_loop.py
+fi
+
 exec "$@"
--- init.sh
+++ init.sh
@@ -5,7 +5,13 @@ if [ -f ./.secrets/secret_api_export.sh ]; then
     source ./.secrets/secret_api_export.sh
 fi
 
-# Docker image and container management
 docker pull ghcr.io/davidudell/persistent:latest
 docker system prune -f
-docker run -it -e OPENAI_API_KEY=$OPENAI_API_KEY ghcr.io/davidudell/persistent:latest
+
+xhost +local:docker
+
+# Trying to hook up X11 inside the container to outside
+docker run -it \
+    -e OPENAI_API_KEY=$OPENAI_API_KEY \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    ghcr.io/davidudell/persistent:latest
