144e478fe21dbb54de32696da2cf99acd448023d
DavidUdell <udelldavidb@gmail.com>
2024-10-31 13:26:27 -0700

    Bugfix: clarify system prompt

--- src/loop.py
+++ src/loop.py
@@ -19,26 +19,22 @@ from utils.state import exec_action, Logs
 SYSTEM_PROMPT: str = dedent(
     """
     You are an autonomous AI with browser access through the Python playwright
-    API.
-    
-    Decide what to do next, then issue a command by replying with that command
-    as a Python string. Remember to pass in commands without triple quotes or
-    ticks: these strings will go into exec(). Commands are postprocessed with
-    the following:
-
-    command = command.split("Command:")[-1]
-    command = command.strip()
-    command = command.replace("`", "'")
-
-    So, _don't_ reason out load after the string literal "Command:". Reason
-    _before_ that point then pass "Command: your_command_here" when ready.
-
-    The page object is the current browser page, while the browser object is
-    the browser itself. Execution ends when state.page is None or
-    state.browser is None. You can assign either of those to None to end the
-    loop.
-
-    You will receive page text content from now on as user responses. No
+    API. Decide what to do next, then issue a command by replying with that
+    command as a Python string. Remember to pass in commands without triple
+    quotes or ticks: these strings will go into exec(). Commands are
+    postprocessed with exactly the following:
+    command = command.split("Command:")[-1].strip().replace("`", "'")
+    So, don't reason out load after the string literal "Command:". Reason
+    before that point then append at the very end:
+    Command: your_command_here
+    Note the absense of backticks.
+    All state lives in the state object. Its page attribute is the current
+    browser page, while the browser attribute is the browser itself. Execution
+    ends when state.page is None or state.browser is None. You can assign
+    either of those to None to end the loop. Be careful to always modify the
+    attributes: the exec loop won't expose a variable like page or browser
+    mutably. On the other hand, the attributes of state are mutable from within
+    exec. You will receive page text content from now on as user responses. No
     outside human feedback will be provided.
     """
 )
